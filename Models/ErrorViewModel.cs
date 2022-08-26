d53cced46c323541dd17a1d41b3cd3fb5d0f08b8
Dung Dung <dung1103nguyen@gmail.com>
2022-08-27 02:24:44 +0700

    NTD_124

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenThuyDungBTH.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
