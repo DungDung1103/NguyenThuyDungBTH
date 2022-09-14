98f8d6ae26cfeed6e64574b9a353d086fc4fdc16
Dung Dung <dung1103nguyen@gmail.com>
2022-09-14 22:32:24 +0700

    NTD_1409

--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -5,15 +5,39 @@ namespace NguyenThuyDungBTH.Controllers
 {
     public class PersonController: Controller
 {
-    [HttpPost]
+    
     public IActionResult Index(Person std)
     {
         ViewBag.Ps = std.PersonID + "-"+ std.PersonName + "-"+ std.PersonBir + "-" + std.Address; 
-        return View();
+       return View();
     }
-    public IActionResult Create ()
+  public class EmployeeController : Controller
     {
-        return View();
+        // Viết các phương thức
+        public IActionResult List()
+{
+    
+    // khoi tao list Student
+     List<Person> listPerson = new List<Person>()
+     {
+         new Person {PersonID = 1, PersonName = "Nguyen Van A", PersonBir="11/03/2001", Address = "Hà Nội"},
+         new Person {PersonID = 2, PersonName = "Nguyen Van B", PersonBir="26/05/2001", Address = "Hải Phòng"},
+         new Person {PersonID = 3, PersonName = "Nguyen Van C", PersonBir="11/12/2001", Address= "Nam Định"}
+         
+     };
+     ViewData["Persons"] = listPerson;
+    return View(listPerson);
+}
+[HttpPost]
+ public IActionResult Create(Person std)
+        {
+            string mess = std.PersonID + "-";
+            mess += std.PersonName + "-";
+            mess += std.PersonBir + "-";
+            mess += std.Address;
+            ViewBag.EMPL = mess;
+            return View();
+        }
     }
 }
 
--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -15,20 +15,20 @@ public class StudentController: Controller
     return View();
 }
     
-public IActionResult Index()
+public IActionResult List()
 {
     
     // khoi tao list Student
-    // List<Student> stdList = new List<Student>()
-    // {
-    //     new Student {StudentID = 1, StudentName = "Nguyen Van A", StudentAge = 18},
-    //     new Student {StudentID = 2, StudentName = "Nguyen Van B", StudentAge = 18},
-    //     new Student {StudentID = 3, StudentName = "Nguyen Van C", StudentAge = 18},
-    //     new Student {StudentID = 4, StudentName = "Nguyen Van D", StudentAge = 18},
-    //     new Student {StudentID = 5, StudentName = "Nguyen Van E", StudentAge = 18}
-    // };
-    // ViewData["Students"] = stdList;
-    return View();
+     List<Student> stdList = new List<Student>()
+     {
+         new Student {StudentID = 1, StudentName = "Nguyen Van A", StudentAge = 18},
+         new Student {StudentID = 2, StudentName = "Nguyen Van B", StudentAge = 18},
+         new Student {StudentID = 3, StudentName = "Nguyen Van C", StudentAge = 18},
+         new Student {StudentID = 4, StudentName = "Nguyen Van D", StudentAge = 18},
+         new Student {StudentID = 5, StudentName = "Nguyen Van E", StudentAge = 18}
+     };
+     ViewData["Students"] = stdList;
+    return View(stdList);
 }
 [HttpPost]
  public IActionResult Create(Student std)
